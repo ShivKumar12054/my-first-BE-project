3cd7d6c5692bb673bc03bf609840a3540c72b38a
Shivakumar M <shivakumar12054@gmail.com>
2025-01-13 16:54:39 +0530

    feat(code-format): fix code format

--- helloserver.js
+++ helloserver.js
@@ -1,16 +1,15 @@
-const express = require('express')
-const myAPP = express()
-const port = 3000
+const express = require("express");
+const myAPP = express();
+const port = 3000;
 
 myAPP.listen(port, (err) => {
-  if(err) {
-    console.error("Erros starting server: ", err)
+  if (err) {
+    console.error("Erros starting server: ", err);
   } else {
-    console.log(`server running on port ${port}`)
+    console.log(`server running on port ${port}`);
   }
-})
+});
 
-myAPP.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+myAPP.get("/", (req, res) => {
+  res.send("Hello World!");
+});
--- simpleAPI.js
+++ simpleAPI.js
@@ -1,33 +1,34 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const PORT = 3000
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const PORT = 3000;
 
-const app = express()
-app.use(cors())
-app.use(express.json())
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("combined"));
 
 const users = [
-    {id: 1, name: 'spiderman', age: 77},
-    {id: 2, name: 'ironman', age: 33},
-     {id: 3, name: 'antman', age: 1091},
-    {id: 4, name: 'thor', age: 33},
-    {id: 4, name: 'Akshay S', age: 25},
-]
+  { id: 1, name: "spiderman", age: 77 },
+  { id: 2, name: "ironman", age: 33 },
+  { id: 3, name: "antman", age: 1091 },
+  { id: 4, name: "thor", age: 33 },
+  { id: 4, name: "Akshay S", age: 25 },
+];
 
 // to log the request to this server
-app.use(morgan('combined'))
 
 app.get("/api/users", (req, res) => {
-    res.json(users)
-})
+  res.json(users);
+});
 
 app.post("/api/users", (req, res) => {
-    const newUser = req.body;
-    users.push(newUser)
-    res.status(201).json(newUser)
-})
+  const newUser = req.body;
+  users.push(newUser);
+  res.status(201).json(newUser);
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
