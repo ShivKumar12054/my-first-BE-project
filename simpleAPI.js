0fe9b3ef97a1d32420011881132e56ced4e6a093
Shivakumar M <shivakumar12054@gmail.com>
2025-01-13 15:24:38 +0530

    feat(morgan-logger): add logger when ever there is a request to this server

--- simpleAPI.js
+++ simpleAPI.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const morgan = require('morgan')
 const cors = require('cors')
 const PORT = 3000
 
@@ -10,8 +11,12 @@ const users = [
     {id: 1, name: 'spiderman', age: 77},
     {id: 2, name: 'ironman', age: 33},
      {id: 3, name: 'antman', age: 1091},
-    {id: 4, name: 'thor', age: 33}
+    {id: 4, name: 'thor', age: 33},
+    {id: 4, name: 'Akshay S', age: 25},
 ]
+
+// to log the request to this server
+app.use(morgan('combined'))
 
 app.get("/api/users", (req, res) => {
     res.json(users)
